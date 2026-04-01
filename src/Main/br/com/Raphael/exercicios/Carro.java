69e379475d8dd07a7c6fd16be8c377946ac5a5fb
Raphael Viana <dev.raphaelviana@gmail.com>
2026-04-01 14:32:31 -0400

    exercicio 4

--- src/Main/br/com/Raphael/exercicios/Carro.java
+++ src/Main/br/com/Raphael/exercicios/Carro.java
@@ -1,9 +1,38 @@
 package Main.br.com.Raphael.exercicios;
 
 public class Carro {
-    String marca;
-    String modelo;
-    int ano;
+    private String marca;
+    private String modelo;
+    private int ano;
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        if (ano < 1886 || ano > 2026){
+            // coloquei para mostrar qual carro esta com ano inválido
+            System.out.println("Erro: O ano " + ano + " é inválido para o carro. Marca: " + this.marca + " - modelo: " + this.modelo);
+        } else {
+            this.ano = ano;
+        }
+    }
 
     public void exibir() {
         System.out.println("Marca: " + marca + " | Modelo: " + modelo + " | Ano: " + ano);
--- src/Main/br/com/Raphael/exercicios/Main.java
+++ src/Main/br/com/Raphael/exercicios/Main.java
@@ -5,24 +5,25 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         Carro meuCarro1 = new Carro();
-        meuCarro1.marca = "Fiat";
-        meuCarro1.modelo = "Uno";
-        meuCarro1.ano = 2012;
+        meuCarro1.setMarca("Fiat");
+        meuCarro1.setModelo("Uno");
+        meuCarro1.setAno(2012);
 
         Carro meuCarro2 = new Carro();
-        meuCarro2.marca = "Honda";
-        meuCarro2.modelo = "HRV";
-        meuCarro2.ano = 2025;
+        meuCarro2.setMarca("Honda");
+        meuCarro2.setModelo("HRV");
+        meuCarro2.setAno(2025);
 
         Carro meuCarro3 = new Carro();
-        meuCarro3.marca = "BMW";
-        meuCarro3.modelo = "320i";
-        meuCarro3.ano = 2026;
+        meuCarro3.setMarca("BMW");
+        meuCarro3.setModelo("320i");
+        meuCarro3.setAno(1600);
 
         ArrayList<Carro>listaCarros = new ArrayList<>();
         listaCarros.add(meuCarro1);
         listaCarros.add(meuCarro2);
         listaCarros.add(meuCarro3);
+
         System.out.println("--- Imprimindo a lista com for-each ---");
         for (Carro carroDaVez : listaCarros) {
             carroDaVez.exibir();
