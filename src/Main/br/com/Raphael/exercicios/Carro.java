b8e13a4417525d6873b7a5f7b915688f94771bd3
Raphael Viana <dev.raphaelviana@gmail.com>
2026-03-31 20:39:55 -0400

    first commit

---
+++ src/Main/br/com/Raphael/exercicios/Carro.java
@@ -0,0 +1,7 @@
+package Main.br.com.Raphael.exercicios;
+
+public class Carro {
+    String marca;
+    String modelo;
+    int ano;
+}
---
+++ src/Main/br/com/Raphael/exercicios/Main.java
@@ -0,0 +1,19 @@
+package Main.br.com.Raphael.exercicios;
+
+public class Main {
+    public static void main(String[] args) {
+        Carro carro1 = new Carro();
+        carro1.marca = "Toyota";
+        carro1.modelo = "Corolla";
+        carro1.ano = 2022;
+
+        Carro carro2 = new Carro();
+        carro2.marca = "Honda";
+        carro2.modelo = "Civic";
+        carro2.ano = 2023;
+
+        System.out.println("Carro 1: " + carro1.marca + " " + carro1.modelo + " " + carro1.ano);
+        System.out.println("Carro 2: " + carro2.marca + " " + carro2.modelo + " " + carro2.ano);
+    }
+}
+
