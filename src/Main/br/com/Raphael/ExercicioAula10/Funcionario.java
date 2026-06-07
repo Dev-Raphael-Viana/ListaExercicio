3ad31213636392ee1ed6391d87929542b7e30a7d
Raphael Viana <dev.raphaelviana@gmail.com>
2026-06-07 17:59:48 -0400

    exercicio 8

---
+++ src/Main/br/com/Raphael/ExercicioAula10/Funcionario.java
@@ -0,0 +1,21 @@
+package main.br.com.Raphael.ExercicioAula10;
+
+class Funcionario {
+    String nome;
+    double salario;
+
+    public Funcionario(String nome, double salario) {
+        this.nome = nome;
+        this.salario = salario;
+    }
+
+    public double calcularBonus() {
+        return this.salario * 0.10;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public double getSalario() {
+        return salario;
+    }
+}
--- src/Main/br/com/Raphael/ExercicioAula10/Main.java
+++ src/Main/br/com/Raphael/ExercicioAula10/Main.java
@@ -2,13 +2,9 @@ package main.br.com.Raphael.ExercicioAula10;
 
 public class Main {
     public static void main(String[] args) {
-        Animal animalGenerico = new Animal("Animal ");
-        Cachorro cachorro = new Cachorro("Rex ");
-        Gato gato = new Gato("Whiskas ");
-
-
-        animalGenerico.emitirSom();
-        cachorro.emitirSom();
-        gato.emitirSom();
+        Funcionario f1 = new Funcionario("Diego", 3000.00);
+        Gerente g1 = new Gerente("Raphael", 7000.00);
+        System.out.println("Bônus do Funcionário " + f1.nome + ": R$ " + f1.calcularBonus());
+        System.out.println("Bônus da Gerente " + g1.nome + ": R$ " + g1.calcularBonus());
     }
 }
