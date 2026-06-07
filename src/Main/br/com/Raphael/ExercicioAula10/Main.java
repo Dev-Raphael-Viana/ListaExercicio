eae89b727ade87bdfa86fe951a487554a911b9d0
Raphael Viana <dev.raphaelviana@gmail.com>
2026-06-07 13:41:55 -0400

    exercicio 4

--- src/Main/br/com/Raphael/ExercicioAula10/Main.java
+++ src/Main/br/com/Raphael/ExercicioAula10/Main.java
@@ -1,22 +1,15 @@
 package main.br.com.Raphael.ExercicioAula10;
 
+import java.math.BigDecimal;
+
 public class Main {
     public static void main(String[] args) {
-       Integer a = 127;
-       Integer b = 127;
-       System.out.println(a == b);
-
-        Integer c = 200;
-        Integer d = 200;
-        System.out.println(c == d);
-        System.out.println(c.equals(d));
+        BigDecimal preco = new BigDecimal("49.90");
+        BigDecimal qtd = new BigDecimal("3");
+        BigDecimal desconto = new BigDecimal("0.95");
+        BigDecimal total = preco.multiply(qtd).multiply(desconto);
+        System.out.println("Total: R$ " + total);
     }
 }
 
-/*
- * O Java possui o "Integer Cache". Para economizar memória, ele reaproveita
- * os mesmos objetos Integer para números de -128 até 127.
- * * Com 127: 'a' e 'b' apontam para o MESMO objeto no cache (por isso == dá true).
- * * Com 200: Está fora do limite do cache. O Java cria dois objetos DIFERENTES
- * na memória por isso == dá false, mas .equals() dá true pelo conteúdo.
- */
+// R$ 142.215
