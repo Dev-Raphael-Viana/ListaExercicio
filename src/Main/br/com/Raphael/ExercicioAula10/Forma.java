396c67a3e7d094ce665362ea56202b87352bf39b
Raphael Viana <dev.raphaelviana@gmail.com>
2026-06-07 18:14:22 -0400

    exercicio 11

--- src/Main/br/com/Raphael/ExercicioAula10/Forma.java
+++
@@ -1,5 +0,0 @@
-package main.br.com.Raphael.ExercicioAula10;
-
-abstract class Forma {
-    public abstract double calcularArea();
-}
--- src/Main/br/com/Raphael/ExercicioAula10/Main.java
+++ src/Main/br/com/Raphael/ExercicioAula10/Main.java
@@ -2,10 +2,16 @@ package main.br.com.Raphael.ExercicioAula10;
 
 public class Main {
     public static void main(String[] args) {
-        Forma circulo = new Circulo(5.0);
-        Forma retangulo = new Retangulo(4.0, 6.0);
+        System.out.println("--- Testando Conta Corrente ---");
+        Conta cc = new ContaCorrente();
+        cc.depositar(100.00);
+        cc.sacar(300.00);
+        cc.sacar(400.00);
 
-        System.out.println("Área do Círculo: " + circulo.calcularArea());
-        System.out.println("Área do Retângulo: " + retangulo.calcularArea());
+        System.out.println("\n--- Testando Conta Poupança ---");
+        Conta poupanca = new ContaPoupanca();
+        poupanca.depositar(100.00);
+        poupanca.sacar(150.00);
+        poupanca.sacar(50.00);
     }
 }
