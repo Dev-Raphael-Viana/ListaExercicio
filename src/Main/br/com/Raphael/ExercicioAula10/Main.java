2c951176885514aaacdb4b82aacbbe572ec53d8d
Raphael Viana <dev.raphaelviana@gmail.com>
2026-06-07 13:23:11 -0400

    exercicio 3

--- src/Main/br/com/Raphael/ExercicioAula10/Main.java
+++ src/Main/br/com/Raphael/ExercicioAula10/Main.java
@@ -2,12 +2,21 @@ package main.br.com.Raphael.ExercicioAula10;
 
 public class Main {
     public static void main(String[] args) {
-        Integer x = null;
-        int y = x;
+       Integer a = 127;
+       Integer b = 127;
+       System.out.println(a == b);
 
-        System.out.println(y);
+        Integer c = 200;
+        Integer d = 200;
+        System.out.println(c == d);
+        System.out.println(c.equals(d));
     }
 }
 
-//NullPointerException. O Java tenta fazer unboxing (converter Integer para int), mas o valor e null.
-//Primitivo não aceita null.
+/*
+ * O Java possui o "Integer Cache". Para economizar memória, ele reaproveita
+ * os mesmos objetos Integer para números de -128 até 127.
+ * * Com 127: 'a' e 'b' apontam para o MESMO objeto no cache (por isso == dá true).
+ * * Com 200: Está fora do limite do cache. O Java cria dois objetos DIFERENTES
+ * na memória por isso == dá false, mas .equals() dá true pelo conteúdo.
+ */
