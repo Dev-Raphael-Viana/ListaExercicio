eadc04788b24701489fad61bdba8acbaa918ceed
Raphael Viana <dev.raphaelviana@gmail.com>
2026-06-08 11:38:30 -0400

    exercicio 12

--- src/Main/br/com/Raphael/ExercicioAula10/Main.java
+++ src/Main/br/com/Raphael/ExercicioAula10/Main.java
@@ -2,16 +2,11 @@ package main.br.com.Raphael.ExercicioAula10;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("--- Testando Conta Corrente ---");
-        Conta cc = new ContaCorrente();
-        cc.depositar(100.00);
-        cc.sacar(300.00);
-        cc.sacar(400.00);
+        Imprimivel notaFiscal = new Nota(350.90);
+        Imprimivel relatorioMensal = new Relatorio("Uso de CPU está em 45%. Tudo estável.");
 
-        System.out.println("\n--- Testando Conta Poupança ---");
-        Conta poupanca = new ContaPoupanca();
-        poupanca.depositar(100.00);
-        poupanca.sacar(150.00);
-        poupanca.sacar(50.00);
+        notaFiscal.imprimir();
+        System.out.println();
+        relatorioMensal.imprimir();
     }
 }
